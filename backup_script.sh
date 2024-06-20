680bb7effc7c286ad9b8d27ce4cf73529aed792b
VladSad55 <154800959+VladSad55@users.noreply.github.com>
2024-06-20 19:20:56 +0300

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+BACKUP_DIR="/path/to/backup"
+SOURCE_DIR="/path/to/source"
+BACKUP_FILE="$BACKUP_DIR/backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
+
+tar -czf $BACKUP_FILE $SOURCE_DIR
+
+echo "Резервна копія створена: $BACKUP_FILE"
 
