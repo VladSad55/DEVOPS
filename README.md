c9d385ae593cedac380bda501b75e33d1e08e493
VladSad55 <154800959+VladSad55@users.noreply.github.com>
2024-06-20 19:26:44 +0300

    Update and rename README.md to backup_script1.sh

--- README.md
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tar -czf backup.tar.gz /path/to/directory
-#!/bin/bash
-find /path/to/directory -type f ! -name "*.dat" -print0 | tar -czvf backup.tar.gz --null -T -
---
+++ backup_script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BACKUP_DIR="/path/to/backup"
+SOURCE_DIR="/path/to/source"
+BACKUP_FILE="$BACKUP_DIR/backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
+
+tar --exclude='*.dat' -czf $BACKUP_FILE $SOURCE_DIR
+
+echo "Резервна копія створена: $BACKUP_FILE"
